d2bc3b431bfc701fc7951a2f55a5a4b87bdeb315
ivan_pogorenko <ipogorenko@yandex.ru>
2025-03-09 23:31:52 +0700

    chat screen

--- app/src/main/java/com/tsu/mobilecourse/UI/activity/ChatActivity.kt
+++ app/src/main/java/com/tsu/mobilecourse/UI/activity/ChatActivity.kt
@@ -1,12 +1,12 @@
 package com.tsu.mobilecourse.UI.activity
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.tsu.mobilecourse.UI.fragment.ChatMainFragment
+import com.tsu.mobilecourse.UI.fragment.PersonChatFragment
 import com.tsu.mobilecourse.databinding.ActivityChatBinding
 
-class ChatActivity : AppCompatActivity(){
+class ChatActivity : AppCompatActivity(), ChatMainFragment.OnFragmentInteractionListener{
 
     private lateinit var binding: ActivityChatBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +15,66 @@ class ChatActivity : AppCompatActivity(){
         val view = binding.root
         setContentView(view)
 
+        val chats = listOf(
+            mapOf(
+                "blockImg" to com.tsu.mobilecourse.R.drawable.profile_image,
+                "blockName" to "Имя",
+                "blocktext" to "Сообщение Сообщение Сообщение Сообщение Сообщение",
+                "id" to "bio"
+            ),
+            mapOf(
+                "blockImg" to com.tsu.mobilecourse.R.drawable.profile_image,
+                "blockName" to "Имя",
+                "blocktext" to "Сообщение Сообщение Сообщение Сообщение Сообщение",
+                "id" to "bio"
+            ),
+        )
+
+        if (savedInstanceState == null) {
+            for (card in chats) {
+                val fragment = ChatMainFragment()
+                val bundle = Bundle().apply {
+                    putInt("blockImg", card["blockImg"] as Int)
+                    putString("blockName", card["blockName"] as String)
+                    putString("blocktext", card["blocktext"] as String)
+                }
+                fragment.arguments = bundle
+                supportFragmentManager.beginTransaction()
+                    .add(binding.chatContainer.id, fragment)
+                    .commit()
+            }
+        }
+
     }
+
+    override fun onFragmentClicked(name: String) {
+        val newFragment = when (name) {
+            "Имя" -> {
+                val fragment = PersonChatFragment()
+                val bundle = Bundle().apply {
+                    putString("name", "Имя человека")
+                    putString("message", "Сообщение для PersonChatFragment")
+                }
+                fragment.arguments = bundle
+                fragment
+            }
+            else -> return
+        }
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        for (fragment in fragmentManager.fragments) {
+            fragmentTransaction.remove(fragment)
+        }
+
+        fragmentTransaction.replace(binding.personContainer.id, newFragment)
+            .commit() // Выполняем транзакцию
+
+        fragmentManager.beginTransaction()
+            .replace(binding.personContainer.id, newFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
 }
